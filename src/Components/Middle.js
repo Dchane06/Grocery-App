2db52934200df672b023152facf0a01519ecbccc
Devan Chaney <dchane06@hotmail.com>
2021-05-16 20:25:57 -0400

    Added click events, delete button, etc.

--- src/Components/Middle.js
+++ src/Components/Middle.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import '../index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBan } from '@fortawesome/free-solid-svg-icons';
+
 
 // State to save items
 
@@ -7,12 +10,66 @@ import '../index.css';
 
 function Middle() {
     const [items, setItems] = useState([
-        { itemName: "item 1", quantity: 1, isSelected: false },
-        { itemName: "item 2", quantity: 2, isSelected: false },
-        { itemName: "item 3", quantity: 3, isSelected: false },
     ]);
 
+    const [inputValue, setInputValue] = useState("");
+
+    const handleAddButtonClick = () => {
+        const newItem = {
+            itemName: inputValue,
+            quantity: 1,
+            isSelected: false,
+        }
+
+        const newItems = [...items, newItem];
+
+        setItems(newItems);
+        setInputValue("");
+    }
+
+    const handleAddQuantityClick = (index) => {
+        // if increase is clicked, pick out object clicked, and then add 1 to quantity.
+        const newItems = [...items];
+
+        // Object's index
+        newItems[index].quantity++;
+
+        setItems(newItems);
+
+    }
+
+    const handleSubtractQuantityClick = (index) => {
+        const newItems = [...items];
+
+        newItems[index].quantity--;
+
+        // if (newItems[index].quantity === 0) {
+        //     let value = 0;
+        //     newItems = newItems.filter(item => item !== value)
+        // }
+        setItems(newItems);
+    }
+
+    const toggleComplete = (index) => {
+        const newItems = [...items];
+        
+        newItems[index].isSelected = !newItems[index].isSelected;
+
+        setItems(newItems);
+    }
+
+    const handleDeleteButtonClick = (index) => {
+        const newItems = [...items];
+
+        newItems.splice(index, 1);
+        setItems(newItems);
+
+        
+
+        setItems(newItems);
+    }
     return (
+        <div class="card">
         <div class="card text-center">
   <div class="card-header">
     List
@@ -22,26 +79,23 @@ function Middle() {
     <p class="card-text">Use the input boxes and button below to create your grocery list.</p>
     <p class="card-text">Put the name of the item in the Item Name box, then put how much of the item you have left in the Quantity Left box.</p>
     <p class="card-text">Use the + or - button beside the Item Name box to add or remove an item. Use the + or - beside the Quantity Left box to add or subtract the quantity left.</p>
+        <div className="add-item-box">
+            <input value={inputValue} onChange={(event)=> setInputValue(event.target.value)} className="add-item-input"></input>
+        <button className="add-item-button" onClick={() => handleAddButtonClick()}>Add to list!</button>
+        </div>
         <div className="item-list">
             {items.map((item, index) => (
-                    <div className="item-name">
-                        {item.isSelected ? (
-                            <>
-                            <span className="completed">{item.itemName}</span>
-                            </>
-                        ) : (
-                            <>
-                                <span>{item.itemName}</span>
-                            </>
-                        )}
+                    <div className="item-name" onClick={() => toggleComplete}>
+                        <span>{item.itemName}</span>
                     
                 <div className="quantity">
-                    <button>-</button>
+                    <button onClick={() => handleSubtractQuantityClick(index)}>-</button>
                     <span> {item.quantity} </span>
-                    <button>+</button>
+                    <button onClick={() => handleAddQuantityClick(index)}>+</button> <button onClick={() => handleDeleteButtonClick(index)}>Delete</button>
                 </div>
         </div>
             ))}
+    </div>
     </div>
     </div>
     </div>
